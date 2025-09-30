fabb7818ecb6365b946158ad6c485155c6ac182c
tranhuynhnhattan96@gmail.com <tranhuynhnhattan96@gmail.com>
2025-09-30 13:48:17 +0700

    (update): OnDone task

--- src/app/components/to-do/to-do.ts
+++ src/app/components/to-do/to-do.ts
@@ -13,16 +13,16 @@ export class TodoComponent {
   });
 
   tasks = [
-    { id: 1, title: 'Wake up' },
-    { id: 2, title: 'Drink Water' },
-    { id: 3, title: 'Study' },
-    { id: 4, title: 'Go to work' },
+    { id: 1, title: 'Wake up', done: true },
+    { id: 2, title: 'Drink Water', done: true },
+    { id: 3, title: 'Study', done: false },
+    { id: 4, title: 'Go to work', done: false },
   ];
 
   onAddTask() {
     const taskName = this.toDoForm.value.taskName?.trim();
     if (taskName) {
-      this.tasks = [{ id: this.tasks.length + 1, title: taskName }, ...this.tasks];
+      this.tasks = [{ id: this.tasks.length + 1, title: taskName, done: false }, ...this.tasks];
       this.toDoForm.reset();
     }
   }
@@ -31,5 +31,9 @@ export class TodoComponent {
     if (confirm('Are you sure to delete this task?')) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
     }
+  }
+
+  onDoneTask(id: number) {
+    this.tasks = this.tasks.map((task) => (task.id === id ? { ...task, done: !task.done } : task));
   }
 }
